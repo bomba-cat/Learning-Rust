61c1cce0ba515a81bd5c2b3b818f5598903c7509
maesherr <l.feuerstein.lf@gmail.com>
2024-03-21 11:50:13 +0100

    made a function

---
+++ tac/target/debug/deps/tac.d
@@ -0,0 +1,5 @@
+C:\Users\lfeue\OneDrive\Dokumente\Coding\Learning-Rust\tac\target\debug\deps\tac.exe: src\main.rs
+
+C:\Users\lfeue\OneDrive\Dokumente\Coding\Learning-Rust\tac\target\debug\deps\tac.d: src\main.rs
+
+src\main.rs:
---
+++ tac/target/debug/tac.d
@@ -0,0 +1,1 @@
+C:\Users\lfeue\OneDrive\Dokumente\Coding\Learning-Rust\tac\target\debug\tac.exe: C:\Users\lfeue\OneDrive\Dokumente\Coding\Learning-Rust\tac\src\main.rs
